0bb720caecf5aed429625dcc7e7bd9b285353c3a
Levi <128552152+dylanabzr@users.noreply.github.com>
2023-08-30 11:59:11 +0000

    Add files via upload

--- Lista2/src/questao06.c
+++ Lista2/src/questao06.c
@@ -3,7 +3,7 @@
 #define NUMC1 5
 #define NUMC2 7
 
-void votos(int voto, int *candidato1, int *candidato2, int *nulo){
+void votos(int voto, int *candidato1, int *candidato2, int *nulo, int *brancos){
         switch(voto){
 	case NUMC1:
 		*candidato1+=1;
@@ -11,6 +11,8 @@ void votos(int voto, int *candidato1, int *candidato2, int *nulo){
 	case NUMC2:
 		*candidato2+=1;
 		break;
+	case 0:
+		*brancos +=1;
 	default:
 		*nulo+=1;
 		break;
@@ -18,19 +20,26 @@ void votos(int voto, int *candidato1, int *candidato2, int *nulo){
 }
 
 int main(){
-	int tmp, candidato1 = 0, candidato2 = 0, nulo = 0;
+	int tmp, candidato1 = 0, candidato2 = 0, nulo = 0, brancos = 0;
+	char SN[3] = "sS";
+	char SouN;
 	while(1){
 		tmp = 0;
 		printf("Digite seu voto: ");
 		scanf("%d", &tmp);
+		printf("Tem certeza [s/n]? ");
+		scanf(" %c", &SouN);
+		if (!(SouN==SN[0] || SouN==SN[1])) continue; 
 		if (tmp<0) break;
-		else votos(tmp, &candidato1, &candidato2, &nulo);
+		else votos(tmp, &candidato1, &candidato2, &nulo, &brancos);
         }
-	int votostotais = candidato1 + candidato2 + nulo;
-	printf("%.2f%% dos votos foram para Paulo, %.2f%% para Renata e %.2f%% foram votos nulos\n", (float)candidato1/votostotais*100, (float)candidato2/votostotais*100, (float)nulo/votostotais*100);
+	int votostotais = candidato1 + candidato2 + nulo + brancos;
+	printf("%.2f%% dos votos foram para Paulo, %.2f%% para Renata, %2.f%% foram votos brancos, %.2f%% foram votos nulos\n", (float)candidato1/votostotais*100, (float)candidato2/votostotais*100, (float)brancos/votostotais*100, (float)nulo/votostotais*100);
 	if (candidato1 != candidato2)
 		printf("O(a) vencedor(a) foi %s\n", candidato1>candidato2?"Paulo":"Renata");
 	else
 		printf("Os candidatos empataram!\n");
 	return 0;
 }
+
+
