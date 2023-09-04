dbca2713811d94f93395b4d32d2d48a1b3ab2997
dylanabzr <dylanab@pm.me>
2023-09-04 19:05:17 -0300

    Deixando mais bonito

--- Exercicios-Classe/bitsligados.c
+++ Exercicios-Classe/bitsligados.c
@@ -5,7 +5,7 @@ int main(){
   unsigned char x = 0b11000110, y;
   for(unsigned char i = BITS; i>0;i--){
     y = x << 1;
-    y = y >> 1;
+    y >>= 1;
     if (y != x)
       printf("ligado = %d\n", i);
     x = x << 1;
