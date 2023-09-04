9503c4bc464ccd5cd94b1d6e8afa7e3b1521c672
dylanabzr <dylanab@pm.me>
2023-09-04 18:54:36 -0300

    Pasta de exercícios feitos em sala adicionada

---
+++ Exercicios-Classe/bitsligados.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define BITS 8
+
+int main(){
+  unsigned char x = 0b11000110, y;
+  for(unsigned char i = BITS; i>0;i--){
+    y = x << 1;
+    y = y >> 1;
+    if (y != x)
+      printf("ligado = %d\n", i);
+    x = x << 1;
+  }
+  return 0;
+}
