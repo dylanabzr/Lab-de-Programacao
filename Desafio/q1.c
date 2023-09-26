cf2f6ee183f35b0886d284b31e14d2dcbe16b8e3
dylanabzr <dylanab@pm.me>
2023-09-26 19:34:15 -0300

    update

--- Desafio/q1.c
+++ Desafio/q1.c
@@ -8,35 +8,38 @@ char verificar(char armario, char pos){
 }
 
 int main (){
-	unsigned char armarios = 0;
-	unsigned char x, y;
-	unsigned char done = 1;
-	while(1){
-	for (unsigned char i = 0; i < 8; i++){
-		if (verificar(armarios, i))
-			printf("Armário %hhu está ocupado\n", i);
-		else
-			printf("Armário %hhu está desocupado\n", i);
+	unsigned char armarios = 0, done = 1, x, y;
+  srand(time(NULL));
+	while(done){
+    system("clear");
+    for (unsigned char i = 0; i < 8; i++){
+      if (verificar(armarios, i))
+        printf("Armário %hhu está ocupado\n", i);
+      else
+        printf("Armário %hhu está desocupado\n", i);
+    }
+    puts("Você tem 3 opções: \n 1. Ocupar um armário aleatório\n 2. Liberar um armário a sua escolha\n 3. Sair");
+    scanf("%hhu", &x);
+    switch(x){
+      default:
+        puts("Opção inválida tente novamente");
+        break;
+      case 1:
+        y = rand()%8;
+        if (!verificar(armarios, y))
+          armarios ^= MASK << y;
+        break;
+      case 2:
+        puts("Escolha um armário para liberar: ");
+          scanf("%hhu", &y);
+          if (verificar(armarios, y))
+            armarios ^= MASK << y;
+        break;
+      case 3:
+        done = 0;
+        break;
+    }
 	}
-	puts("Você tem 3 opções: \n 1. Ocupar um armário aleatório\n 2. Liberar um armário a sua escolha\n 3. Sair");
-	scanf("%hhu", &x);
-	if (x == 3)
-		break;
-	if((x > 3)||(x < 1)){
-		puts("Opção inválida tente novamente");
-	}
-	if(x == 1){
-		srand(time(NULL));
-		y = rand()%8;
-		printf("%hhu", y);
-		if (!verificar(armarios, y))
-			armarios ^= MASK << y;
-	}
-	if(x == 2){
-		puts("Escolha um armário para liberar: ");
-			scanf("%hhu", &y);
-			if (verificar(armarios, y))
-				armarios ^= MASK << y;
-	}
-	}
+  system("clear");
+  return 0;
 }
