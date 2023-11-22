b17e6e80741d9fbe69ae46c321a69c09ad5d574d
dylanabzr <dylanab@pm.me>
2023-11-22 06:22:06 -0300

    update

---
+++ Desafio/Image-Filtering/src/Filter.c
@@ -0,0 +1,35 @@
+#include "../include/Filter.h"
+
+unsigned char matrix_average(unsigned char matrix[3][3]){
+  int sum = 0;
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      sum += matrix[i][j];
+    }
+  }
+  return sum/9;
+}
+void filter3x3_average(struct Image *image){
+  for (unsigned i = image->w; i < (image->h - 1) * image->w; i++){
+    if (i % image->w != 0 && (i + 1) % image->w != 0) {
+      unsigned char matrix[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+      matrix[0][0] = image->data[i - image->w - 1];
+      matrix[1][0] = image->data[i - 1];
+      matrix[2][0] = image->data[i + image->w - 1];
+
+      matrix[0][2] = image->data[i - image->w + 1];
+      matrix[1][2] = image->data[i + 1];
+      matrix[2][2] = image->data[i + image->w + 1];
+
+      int sum_index = -image->w;
+      for (int j = 0; j < 3; j++){
+        matrix[j][1] = image->data[i + sum_index];
+        sum_index += image->w;
+      }
+
+      image->data[i] = matrix_average(matrix);
+    }
+  }
+}
+
--- Desafio/Image-Filtering/src/main.c
+++ Desafio/Image-Filtering/src/main.c
@@ -1,9 +1,11 @@
 #include "../include/Image.h"
+#include "../include/Filter.h"
 
 int main (){
   struct Image imagem;
   Image(&imagem, 715, 1024, 1);
   read("mamografia-715x1024.jpg", &imagem);
-  write("mamografia-715x1024.png", &imagem);
+  filter3x3_average(&imagem);
+  write("mamografia-715x1024.bmp", &imagem);
   freeIMG(&imagem);
 }
