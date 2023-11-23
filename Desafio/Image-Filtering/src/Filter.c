1f51c29c00db31753dff5638fdd5b1eec1b3e518
dylanabzr <dylanab@pm.me>
2023-11-23 13:43:32 -0300

    update

--- Desafio/Image-Filtering/src/Filter.c
+++ Desafio/Image-Filtering/src/Filter.c
@@ -16,21 +16,15 @@ struct Image filter3x3_average(struct Image *image) {
   for (unsigned i = image->w; i < (image->h - 1) * image->w; i++){
     if (i % image->w != 0 && (i + 1) % image->w != 0) {
       unsigned char matrix[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-
-      matrix[0][0] = image->data[i - image->w - 1];
-      matrix[1][0] = image->data[i - 1];
-      matrix[2][0] = image->data[i + image->w - 1];
-
-      matrix[0][2] = image->data[i - image->w + 1];
-      matrix[1][2] = image->data[i + 1];
-      matrix[2][2] = image->data[i + image->w + 1];
-
-      int sum_index = -image->w;
-      for (int j = 0; j < 3; j++){
-        matrix[j][1] = image->data[i + sum_index];
-        sum_index += image->w;
+      int posX = -1;
+      for (int x = 0; x < 3; x++){
+        int posY = -image->w;
+        for (int y = 0; y < 3; y++){
+          matrix[y][x] = image->data[i + posY + posX];
+          posY += image->w;
+        }
+        posX++;
       }
-
       filtered_image.data[i] = matrix_average(matrix);
     }
   }
--- Desafio/Image-Filtering/src/main.c
+++ Desafio/Image-Filtering/src/main.c
@@ -6,6 +6,6 @@ int main (){
   struct Image imagem = readPGM("lena.pgm");
   struct Image filtrada = filter3x3_average(&imagem);
   savePGM("lena_filtered.pgm", &filtrada);
-  unsigned char **matriz = CreateSCM(imagem, filtrada, 8);
-  printMatrix(matriz, 8);
+  unsigned char **matriz = CreateSCM(imagem, filtrada, 16);
+  printMatrix(matriz, 16);
 }
